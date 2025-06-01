d6e4fa115c0912d04625eb40e45c648d84954745
Denise <denisemorales8764@gmail.com>
2025-05-31 23:42:12 -0600

    cambios en server.js

--- server.js
+++ server.js
@@ -11,8 +11,7 @@ app.use(bodyParser.json());
 app.use(express.static(__dirname));
 
 mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
+  useUnifiedTopology: true,
 })
 .then(() => console.log('Conexion exitosa'))
 .catch((error) => console.error('Error al conectarse', error));
